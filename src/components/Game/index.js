937667a0c727c1b126494ce4bdea47df27553617
Mario Galindo <mariogalindo89@gmail.com>
2020-04-19 13:17:31 -0500

    Finished with all the visuals without footer

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,13 +1,32 @@
 import React, { Component } from "react";
 import Nav from "../Nav";
 import Header from "../Header"
+import Container from "../Container"
+import ClickItem from "../ClickItem";
+import data from "../../characters.json"
 
 class Game extends Component {
+    state = {
+        data,
+        score: 0,
+        topScore: 0
+    }
     render() {
         return (
             <div>
                 <Nav  />
                 <Header/>
+                <Container>
+                    {this.state.data.map(item => (
+                        <ClickItem
+                          key={item.id}
+                          id={item.id}
+                          shake={!this.state.score && this.state.topScore}
+                          handleClick={this.handleItemClick}
+                          image={item.image}
+                        />
+                    ))}
+                </Container>
 
             </div>
         )
