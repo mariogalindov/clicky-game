7c712e56035d79eb51368d7641cd43b53d682fec
Mario Galindo <mariogalindo89@gmail.com>
2020-04-19 15:13:30 -0500

    Updates

---
+++ precache-manifest.ac5784378ee04164ad636f1b53e13c80.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3337bcde2e45666e72d42fbcc8c1bf1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fab05d51e5674345b8fe",
+    "url": "/static/css/main.73568b72.chunk.css"
+  },
+  {
+    "revision": "b8ba8874f7636b6c31ed",
+    "url": "/static/js/2.8f4aa6e0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8f4aa6e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fab05d51e5674345b8fe",
+    "url": "/static/js/main.2fe2c4e8.chunk.js"
+  },
+  {
+    "revision": "9a5b2e5b1d4bb7e6d417",
+    "url": "/static/js/runtime-main.dfdaa3ec.js"
+  }
+]);
