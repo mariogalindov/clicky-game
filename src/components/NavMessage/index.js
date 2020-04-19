6ed66148adf9bd8c192c55673968b0362653dfed
Mario Galindo <mariogalindo89@gmail.com>
2020-04-19 14:16:52 -0500

    Finished functionality

--- src/components/NavMessage/index.js
+++ src/components/NavMessage/index.js
@@ -7,9 +7,21 @@ class NavMessage extends Component {
         animating: false
     }
 
+    componentDidUpdate({ score, topScore }, prevState) {
+      const newState = { animating: true};
 
+      if (score === 0 && topScore === 0) {
+        newState.message = "";
+      } else if (score === 0 && topScore > 0) {
+        newState.message = "incorrect";
+      } else {
+        newState.message = "correct";
+      }
 
-
+      if (score !== this.props.score || this.state.message !== newState.message) {
+        this.setState(newState);
+      }
+    }
 
     renderMessage = () => {
         switch (this.state.message) {
